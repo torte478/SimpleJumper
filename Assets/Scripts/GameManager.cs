73c63748c8f45dd59ac3d7f7b6ff7b224d94028d
Дмитрий Данилин <d.danilin@bars-open.ru>
2022-04-09 20:17:27 +0300

    rfc(*): update editor version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject platformPrefab;
     void Start()
     {
-        // var platforms = new ObjectPool<GameObject>();
+        // TODO : dispose
+        var platforms = new ObjectPool<GameObject>(
+            () => Instantiate(platformPrefab),
+            (obj) => obj.SetActive(true),
+            (obj) => obj.SetActive(false),
+            (obj) => Destroy(obj),
+            false,
+            10,
+            10);
+
+        for (var i = 0; i < 5; ++i)
+        {
+            var x = Random.Range(-2.5f, 2.5f);
+            var y = -4.0f + i;
+
+            var platform = platforms.Get();
+            platform.transform.position = new Vector3(x, y);
+        }
     }
 
     // Update is called once per frame
