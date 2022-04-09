e77403ef642f5e1fc49aaac5a24f0765deb94d93
Дмитрий Данилин <d.danilin@bars-open.ru>
2022-04-09 19:35:29 +0300

    ftr(SampleScene): add base sprites and GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        // var platforms = new ObjectPool<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
