60fe8acea9a78a48d303766638901b1223ddb9da
Дмитрий Данилин <d.danilin@bars-open.ru>
2022-04-10 11:16:12 +0300

    ftr(Player): add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D body;
-    private float raycastDistance;
+    private Vector2 raycastBounds;
 
     private float yPrevious;
 
     public float JumpForceY;
+    public float MaxSpeed = 3.0f;
+    public float Speed = 50.0f;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        raycastDistance = GetComponent<BoxCollider2D>().bounds.extents.y + .1f;
+
+        var collider = GetComponent<BoxCollider2D>();
+        raycastBounds = new Vector2(
+            collider.bounds.extents.x,
+            collider.bounds.extents.y + 0.1f);
     }
 
     void Update()
@@ -23,9 +29,41 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        var xMove = Input.GetAxis("Horizontal");
+        if (xMove != 0)
+        {
+            var xSpeed = Mathf.Abs(xMove * body.velocity.x);
+
+            if (xSpeed < MaxSpeed)
+            {
+                var force = Speed * xMove * Vector3.right;
+                body.AddForce(force);
+            }
+
+            if (Mathf.Abs(body.velocity.x) > MaxSpeed)
+            {
+                var velocity = new Vector2(
+                    Mathf.Sign(body.velocity.x) * MaxSpeed,
+                    body.velocity.y);
+
+                body.velocity = velocity;
+            }
+        }
+        else
+        {
+            var velocity = body.velocity;
+            velocity.x *= 0.9f;
+            body.velocity = velocity;
+        }
+
         var yCurrent = transform.position.y;
 
-        if (Physics2D.Raycast(transform.position, Vector3.down, raycastDistance) && yCurrent < yPrevious)
+        var left = new Vector3(transform.position.x - raycastBounds.x, yCurrent);
+        var right = new Vector3(transform.position.x + raycastBounds.x, yCurrent);
+
+        if ((Physics2D.Raycast(left, Vector3.down, raycastBounds.y) 
+            || Physics2D.Raycast(right, Vector3.down, raycastBounds.y)) 
+            && yCurrent < yPrevious)
         {
             body.velocity = new Vector2(body.velocity.x, 0);
             body.AddForce(new Vector2(0, JumpForceY));
